c0628398e52e54b4cc9390fffcc0507377e2af60
Gonzalo Duval <gonzaloduval95@hotmail.com>
2020-09-09 10:06:17 -0300

    Ejercicio leer archivo txt en @PostConstruct

--- spring-demo-annotations/src/com/luv2code/practicedemo/FileFortuneService.java
+++ spring-demo-annotations/src/com/luv2code/practicedemo/FileFortuneService.java
@@ -3,9 +3,12 @@ package com.luv2code.practicedemo;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Component;
 
@@ -19,23 +22,30 @@ public class FileFortuneService implements FortuneService {
 	private Random myRandom = new Random();
 	
 	
-	public FileFortuneService() {
+	@PostConstruct
+	private void loadTheFortunesFile() {
+		
+		System.out.println(">> FileFortuneService: inside method loadTheFortunesFile");
+
 		File theFile = new File(fileName);
 		
 		System.out.println("Reading fortunes from file: " + theFile);
 		System.out.println("File exists: " + theFile.exists());
 		
+		// initialize array list
 		theFortunes = new ArrayList<String>();
 		
-		try(BufferedReader br = new BufferedReader(
-				new FileReader(theFile))){
-			
+		// read fortunes from file
+		try (BufferedReader br = new BufferedReader(
+				new FileReader(theFile))) {
+
 			String tempLine;
-			
-			while((tempLine=br.readLine())!=null)
+
+			while ((tempLine = br.readLine()) != null) {
 				theFortunes.add(tempLine);
+			}
 			
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
