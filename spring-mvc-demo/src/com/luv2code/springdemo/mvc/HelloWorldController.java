0ec40815f3143d9432df1791e26a007df5f3182d
Gonzalo Duval <gonzaloduval95@hotmail.com>
2020-09-14 10:47:08 -0300

    Binding Request Params (Annotation)

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -22,7 +23,7 @@ public class HelloWorldController {
 	}
 	
 	// new controller method to read form data and add data to the model
-	@RequestMapping("/processFormVersion2")
+	@RequestMapping("/processFormVersionTwo")
 	public String letsShouetDude(HttpServletRequest request, Model model) {
 		
 		// read the request parameter from the HTML form
@@ -33,6 +34,29 @@ public class HelloWorldController {
 		
 		// create the message
 		String result = "Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		/* Esto ya no es necesario, spring lo va a hacer automáticamente "behaind the scenes" gracias a la
+		 * anotación agregada en los parámetros de este método
+		String theName = request.getParameter("studentName");
+		*/
+		
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey My Friend from v3! " + theName;
 		
 		// add message to the model
 		model.addAttribute("message", result);
