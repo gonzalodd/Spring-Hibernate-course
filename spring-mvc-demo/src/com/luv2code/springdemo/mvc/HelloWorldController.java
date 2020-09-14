a67efbf249dcd77a7c432883b254f40afc8a9011
Gonzalo Duval <gonzaloduval95@hotmail.com>
2020-09-14 10:59:33 -0300

    Controller level request mapping
    Recreamos un posible caso de error de mapeo y utilizamos el requestmapping a nivel controller para solucionarlo

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -8,16 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello") // Parent maping, con esto todo lo que este dentro de esta clase va a ser relativo
+						  // a hello, algo parecido a subcarpetas de hello
 public class HelloWorldController {
 	
 	// need a controller method to show the initial HTML form
-	@RequestMapping("/showForm")
+	@RequestMapping("/showForm") // complete path = hello/showForm
 	public String showForm() {
 		return "helloworld-form";
 	}
 	
 	// need a controller method to process the HTML form
-	@RequestMapping("/processForm")
+	@RequestMapping("/processForm") // complete path = hello/processform
 	public String processForm() {
 		return "helloworld";
 	}
