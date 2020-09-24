bd05871962edd9543133e1e1c2c709ce28bb0ee7
Gonzalo Duval <gonzaloduval95@hotmail.com>
2020-09-24 12:07:08 -0300

    AOP - Pointcut Expressions practice 4

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -13,7 +13,8 @@ public class MyDemoLoggingAspect {
 	
 	// let's start with an @Before advice
 	
-	@Before("execution(public void add*())")
+	//@Before("execution(public void add*())")
+	@Before("execution(* add*())")
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("\n=====>>> Executing @Before advice on method");
--- spring-demo-aop/src/com/luv2code/aopdemo/dao/MembershipDAO.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class MembershipDAO {
 
-	public void addSillyAccount() {
+	public boolean addSillyAccount() {
 		
 		System.out.println(getClass() + ": DOING STUFF: ADDING A MEMBERSHIP ACCOUNT");
+		
+		return true;
 	}
 	
 }
