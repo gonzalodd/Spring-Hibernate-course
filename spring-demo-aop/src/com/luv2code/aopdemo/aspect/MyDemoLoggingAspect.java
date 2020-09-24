3c34ca2ea9445c5468ca7937360fe9c366ad380b
Gonzalo Duval <gonzaloduval95@hotmail.com>
2020-09-24 12:24:54 -0300

    AOP - Pointcut Expressions practice 5

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -10,11 +10,9 @@ public class MyDemoLoggingAspect {
 	
 	// this is where we add all of our related advice for logging
 	
-	
 	// let's start with an @Before advice
 	
-	//@Before("execution(public void add*())")
-	@Before("execution(* add*())")
+	@Before("execution(* add*(com.luv2code.aopdemo.Account))")
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("\n=====>>> Executing @Before advice on method");
