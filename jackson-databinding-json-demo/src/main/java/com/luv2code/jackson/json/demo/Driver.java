649a58e2ed69fe9fae3b88ae06ad2b9746a47dd3
Gonzalo Duval <gonzaloduval95@hotmail.com>
2020-10-01 11:51:35 -0300

    Spring REST - Display Nested and Arrays

--- jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -15,11 +15,22 @@ public class Driver {
 			
 			// read JSON file and map/convert to Java POJO: data/sample-lite.json
 			Student theStudent = 
-					mapper.readValue(new File("data/sample-lite.json"), Student.class);
+					mapper.readValue(new File("data/sample-full.json"), Student.class);
 			
 			// print first name and last name
 			System.out.println("First name: " + theStudent.getFirstName());
 			System.out.println("Last name: " + theStudent.getLastName());
+			
+			// print out address: street and city
+			Address tempAddress = theStudent.getAddress();
+			
+			System.out.println("Street = " + tempAddress.getStreet());
+			System.out.println("City = " + tempAddress.getCity());
+			
+			// print out laguages
+			for(String tempLang : theStudent.getLanguages()) {
+				System.out.println(tempLang);
+			}
 			
 		} catch (Exception ex) {
 			ex.printStackTrace();
